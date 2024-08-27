a0f15e2519ba4cf5f7cbb6cf96191b5c9cded8ef
nikilandgello <nikoscha22012004@gmail.com>
2024-08-27 15:57:04 +0300

    basics of functions

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function getElementWidth(content, padding, border) {
+    const value = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
+
+    return value;
+};
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
